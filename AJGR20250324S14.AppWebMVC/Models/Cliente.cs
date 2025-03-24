5d16f5f5edaacb770d6773b76b7811af73ba736e
Anderson Galicia <Anderssnjavrmz1@outlook.es>
2025-03-24 15:25:59 -0600

    Se modificaron las vistas de agregar, eliminar, editar y crear

--- AJGR20250324S14.AppWebMVC/Models/Cliente.cs
+++ AJGR20250324S14.AppWebMVC/Models/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace AJGR20250324S14.AppWebMVC.Models;
 
@@ -7,10 +8,15 @@ public partial class Cliente
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "El nombre del cliente es obligatorio.")]
     public string Nombre { get; set; } = null!;
 
+    [Required(ErrorMessage = "La dirección del cliente es obligatoria.")]
+    [Display(Name = "Dirección")]
     public string? Direccion { get; set; }
 
+    [Required(ErrorMessage = "El teléfono del cliente es obligatorio.")]
+    [Display(Name = "Teléfono")]
     public string? Telefono { get; set; }
 
     public string? Email { get; set; }
